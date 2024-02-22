58b00c40fda079e214ce640692213917fba1ddc6
NTTPS <teamerze@gmail.com>
2024-02-22 14:59:13 +0700

    fix

--- pages/borrow.vue
+++ pages/borrow.vue
@@ -511,7 +511,7 @@
                     click: () => fetchEditData(row.req_id, false, true)
                 })
             }
-            if((row.status == 'ส่งมอบใช้งาน' || row.status == 'รายการคงค้าง')) {
+            if((row.status == 'ส่งมอบใช้งาน' || row.status == 'รายการคงค้าง') && !(auth.user.userInGroups.some(g => g.userGroupId === 'ผู้ตรวจสอบยืมพัสดุประจำ ทศ.' && g.isInGroup === true))) {
                 btn.push( {
                     label: 'ขยายเวลายืม-คืน',
                     icon: 'i-heroicons-archive-box-20-solid',
