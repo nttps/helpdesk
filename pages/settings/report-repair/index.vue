729258b8cd01035e9ed52fb596acec234717e2a7
NTTPS <teamerze@gmail.com>
2024-03-07 08:25:11 +0700

    hidden

--- pages/settings/report-repair/index.vue
+++ pages/settings/report-repair/index.vue
@@ -4,10 +4,10 @@
 
         <div class="flex flex-col h-full items-center justify-center"> 
             <div class="flex w-full justify-center space-x-10">
-                <NuxtLink to="/settings/report-repair/purpose" class="p-6 text-center border rounded-xl w-1/6">
+                <!-- <NuxtLink to="/settings/report-repair/purpose" class="p-6 text-center border rounded-xl w-1/6">
                     <Icon name="icon-park-solid:surveillance-cameras-two" size="80" color="gray" />
                     <div class="mt-2">วัตถุประสงค์</div>
-                </NuxtLink>
+                </NuxtLink> -->
                 <NuxtLink to="/settings/report-repair/contact" class="p-6 text-center border rounded-xl w-1/6">
                     <Icon name="i-ri-ticket-fill" size="80" color="gray" />
                     <div class="mt-2">ช่องทางติดต่อ</div>
