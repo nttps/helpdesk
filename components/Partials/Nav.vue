943cee9cd874c84ab0699f565b1bfc0ada244973
NTTPS <teamerze@gmail.com>
2024-03-07 21:29:40 +0700

    return left side

--- components/Partials/Nav.vue
+++ components/Partials/Nav.vue
@@ -1,102 +1,98 @@
 <template>
-    <div class="flex items-center justify-between gap-3 py-2 px-2 border-b-2 border-[#FFA825]">
-        <div class="lg:flex-1 ">
-            <NuxtLink class="flex items-center gap-1.5">
-                <img src="~/assets/images/logo.png" class="w-[70px]" alt="ระบบขอรับบริการด้าน IT Support">
-                <div class="text-xl 2xl:text-2xl ml-2">
-                    <div>ระบบขอรับบริการด้าน IT Support</div> 
-                    <div class="text-[#FFA133]">กรมป้องกันและบรรเทาสาธารณภัย</div> 
-                </div>
-            </NuxtLink>
+    <div class="flex flex-col justify-between h-full">
+        <div>
+            <nav class="nav-top">
+                <ul>
+                    <li>
+                        <NuxtLink to="/" v-slot="{ isActive }">
+                            <Icon name="i-ri-ticket-fill" size="40" :class="isActive ? 'text-black' : ''" />
+                            <div :class="isActive ? 'text-black' : ''">แดชบอร์ด</div>
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/borrow" v-slot="{ isActive }">
+                            <Icon name="i-ri-ticket-fill" size="40" :class="isActive ? 'text-black' : ''" />
+                            <div :class="isActive ? 'text-black' : ''">ยืม-คืนพัสดุ</div>
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/cctv" v-slot="{ isActive }">
+                            <Icon name="icon-park-solid:surveillance-cameras-two" size="40" :class="isActive ? 'text-black' : ''"/>
+                            <div :class="isActive ? 'text-black' : ''">คำขอดู CCTV</div>
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/report-repair" v-slot="{ isActive }">
+                            <Icon name="ic:baseline-handyman" size="40" :class="isActive ? 'text-black' : ''"/>
+                            <div :class="isActive ? 'text-black' : ''">แจ้งปัญหา</div>
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/inventory" v-slot="{ isActive }">
+                            <Icon name="bi:box-seam-fill" size="40" :class="isActive ? 'text-black' : ''"/>
+                            <div :class="isActive ? 'text-black' : ''">คลังอุปกรณ์</div>
+                        </NuxtLink>
+                    </li>
+                    <!-- <li>
+                        <NuxtLink to="/dashboard" v-slot="{ isActive }">
+                            <Icon name="mdi:monitor-dashboard" size="40" :class="isActive ? 'text-black' : ''"/>
+                            <div :class="isActive ? 'text-black' : ''">แดชบอร์ด</div>
+                        </NuxtLink>
+                    </li> -->
+                </ul>
+            </nav>
         </div>
-        <ul class="flex justify-between gap-4">
-            <li class="text-center">
-                <NuxtLink to="/" v-slot="{ isActive }">
-                    <Icon name="i-ri-ticket-fill" size="40" :class="!isActive ? 'text-black' : 'text-[#FFA825]'" />
-                    <div :class="!isActive ? 'text-black' : 'text-[#FFA825]'">แดชบอร์ด</div>
-                </NuxtLink>
-            </li>
-            <li class="text-center">
-                <NuxtLink to="/borrow" v-slot="{ isActive }">
-                    <Icon name="i-ri-ticket-fill" size="40" :class="!isActive ? 'text-black' : 'text-[#FFA825]'" />
-                    <div :class="!isActive ? 'text-black' : 'text-[#FFA825]'">ยืม-คืนพัสดุ</div>
-                </NuxtLink>
-            </li>
-            <li class="text-center">
-                <NuxtLink to="/cctv" v-slot="{ isActive }">
-                    <Icon name="icon-park-solid:surveillance-cameras-two" size="40" :class="!isActive ? 'text-black' : 'text-[#FFA825]'"/>
-                    <div :class="!isActive ? 'text-black' : 'text-[#FFA825]'">คำขอดู CCTV</div>
-                </NuxtLink>
-            </li>
-            <li class="text-center">
-                <NuxtLink to="/report-repair" v-slot="{ isActive }">
-                    <Icon name="ic:baseline-handyman" size="40" :class="!isActive ? 'text-black' : 'text-[#FFA825]'"/>
-                    <div :class="!isActive ? 'text-black' : 'text-[#FFA825]'">แจ้งปัญหา</div>
-                </NuxtLink>
-            </li>
-            <li class="text-center">
-                <NuxtLink to="/inventory" v-slot="{ isActive }">
-                    <Icon name="bi:box-seam-fill" size="40" :class="!isActive ? 'text-black' : 'text-[#FFA825]'"/>
-                    <div :class="!isActive ? 'text-black' : 'text-[#FFA825]'">คลังอุปกรณ์</div>
-                </NuxtLink>
-            </li>
-            <!-- <li>
-                <NuxtLink to="/dashboard" v-slot="{ isActive }">
-                    <Icon name="mdi:monitor-dashboard" size="40" :class="!isActive ? 'text-black' : ''"/>
-                    <div :class="!isActive ? 'text-black' : ''">แดชบอร์ด</div>
-                </NuxtLink>
-            </li> -->
-        </ul>
-    
-        <ul class="flex items-center justify-end lg:flex-1 py-2 gap-4">
-            <li>
-                <button type="button" class="relative" @click="notificationBar = true" ref="buttonNotificationRef">
-                    <UChip :text="notifications.length" color="red" size="2xl" class="flex-col">
-                        <Icon name="material-symbols:notifications" size="40"/>
-                        <div>แจ้งเตือน</div>
-                    </UChip>
-                </button>
-            </li>
-            <li class="text-center">
-                <NuxtLink to="/help" v-slot="{ isActive }" >
-                    <Icon name="material-symbols:help" size="40" :class="!isActive ? 'text-black' : 'text-[#FFA825]'"/>
-                    <div :class="!isActive ? 'text-black' : 'text-[#FFA825]'">ช่วยเหลือ</div>
-                </NuxtLink>
-            </li>
-            <li v-if="authStore.isAdmin">
-                <NuxtLink to="/settings" v-slot="{ isActive }">
-                    <Icon name="material-symbols:settings" size="40" :class="!isActive ? 'text-black' : 'text-[#FFA825]'"/>
-                    <div :class="!isActive ? 'text-black' : 'text-[#FFA825]'">ตั้งค่า</div>
-                </NuxtLink>
-            </li>
-            <li >
-                <a class="text-center">
-                    <UPopover mode="hover" :popper="{ offsetDistance: -15 }">
-                        <UAvatar
-                            :alt="authStore.fullName"
-                            size="md"
-                            class="mx-auto"
-                        />
-                        <template #panel>
-                                <div class="px-4">
-                                    <button 
-                                        class="py-2 px-2 flex items-center space-x-4 border-l-8 border-transparent text-black" 
-                                        exact-active-class="!border-amber-600" 
-                                        active-class="!border-amber-600"
-                                        @click="logout"
-                                    >
-                                        <div>ออกจากระบบ</div>
-                                    </button>
-                                </div>
-                        </template>
-                    </UPopover>
-                    <div class="text-center">
-                        {{ authStore.username }}
-                    </div>
-                </a>
-                
-            </li>
-        </ul>
+
+        <nav class="nav-bottom">
+            <ul>
+                <li>
+                    <button type="button" class="relative" @click="notificationBar = true" ref="buttonNotificationRef">
+                        <UChip :text="notifications.length" color="red" size="2xl" class="flex-col">
+                            <Icon name="material-symbols:notifications" size="40"/>
+                            <div>แจ้งเตือน</div>
+                        </UChip>
+                    </button>
+                </li>
+                <li>
+                    <NuxtLink to="/help" v-slot="{ isActive }">
+                        <Icon name="material-symbols:help" size="40" :class="isActive ? 'text-black' : ''"/>
+                        <div :class="isActive ? 'text-black' : ''">ช่วยเหลือ</div>
+                    </NuxtLink>
+                </li>
+                <li v-if="authStore.isAdmin">
+                    <NuxtLink to="/settings" v-slot="{ isActive }">
+                        <Icon name="material-symbols:settings" size="40" :class="isActive ? 'text-black' : ''"/>
+                        <div :class="isActive ? 'text-black' : ''">ตั้งค่า</div>
+                    </NuxtLink>
+                </li>
+                <li>
+                    <a>
+                        <UPopover mode="hover" :popper="{ offsetDistance: -15 }">
+                            <UAvatar
+                                :alt="authStore.fullName"
+                                size="md"
+                            />
+                            <template #panel>
+                                    <div class="px-4">
+                                        <button 
+                                            class="py-2 px-2 flex items-center space-x-4 border-l-8 border-transparent text-black" 
+                                            exact-active-class="!border-amber-600" 
+                                            active-class="!border-amber-600"
+                                            @click="logout"
+                                        >
+                                            <div>ออกจากระบบ</div>
+                                        </button>
+                                    </div>
+                            </template>
+                        </UPopover>
+                        <div class="text-center">
+                            {{ authStore.username }}
+                        </div>
+                    </a>
+                   
+                </li>
+            </ul>
+        </nav>
     </div>
     <USlideover v-model="notificationBar">
         <Notification :notifications="notifications || []" @refresh="refresh"/>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
-  <PartialsNav />
-  <div class="bg-[#FFA825] px-10  pt-4">
-      
-    <div class="mx-auto p-4 min-h-[calc(100vh-98px-1rem)] bg-white rounded-xl" role="main" aria-label="เนื้อหา">
+  <!-- <div class="bg-[#FFA825] px-2 py-2">
+      <NuxtLink class="flex items-center gap-1.5">
+        <img src="~/assets/images/logo.png" class="w-[70px]" alt="ระบบขอรับบริการด้าน IT Support">
+        <div class="text-xl 2xl:text-2xl ml-2">
+            <div>ระบบขอรับบริการด้าน IT Support</div> 
+            <div class="text-white">กรมป้องกันและบรรเทาสาธารณภัย</div> 
+        </div>
+    </NuxtLink>
+  </div> -->
+  <div class="flex flex-grow h-screen overflow-hidden">
+    <div class="shrink-0 w-20 bg-[#FFA825] text-white pb-4" role="navigation" aria-label="เมนู">
+      <PartialsNav />
+    </div>
+    <div class="shrink-0 flex-grow min-w-[calc(100%-5rem)] max-w-full overflow-auto h-screen  py-10 px-8" role="main" aria-label="เนื้อหา">
       <slot />
     </div>
   </div>
